5ea5eefd30aeb5dd4b6c552cfbd766b49b080539
Pierry <pierryboettscher12@gmail.com>
2024-06-22 15:21:28 -0300

    TesteLambda

---
+++ TesteLambda/TesteLambda/TesteLambda.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace TesteLambda
+{
+    public class LambdaHandler
+    {
+        public static APIGatewayProxyResponse HandleRequest()
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = "Teste deu certo!"
+            };
+        }
+    }
+}
