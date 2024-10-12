51850c9e604d464c9c9ce87db3159b4ca361c0cc
Pierry <pierryboettscher12@gmail.com>
2024-10-12 18:43:19 -0300

    Alterações Download Fatura e Testes Unitários

---
+++ FaturaPdfCatolica/Application/Services/GeracaoPdfService.cs
@@ -0,0 +1,15 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+namespace Application.Services;
+
+public class GeracaoPdfService
+{
+    public byte[] GerarPDF(string pdf)
+    {
+        byte[] bytespdf = File.ReadAllBytes(pdf);
+        MemoryStream ms = new(bytespdf);
+        return ms.ToArray();
+    }
+}
