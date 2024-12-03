32611a9dac1394ae412a918dfc2ddf24f3355e74
PierryB <pierryboettscher12@gmail.com>
2024-12-02 21:07:36 -0300

    Criado RPA Consulta CNPJ

---
+++ RPAs/ConsultaCnpj/Application/Services/ConsultaCnpjService.cs
@@ -0,0 +1,68 @@
+﻿using PuppeteerSharp;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace Application.Services;
+
+public class ConsultaCnpjService (StreamWriter logFile, StreamWriter csvFile, string baseUrl, List<string> listaCnpjs)
+{
+    private StreamWriter LogFile { get; set; } = logFile;
+    private StreamWriter CsvFile { get; set; } = csvFile;
+    private string Url { get; set; } = baseUrl;
+    private List<string> ListaCnpjs { get; set; } = listaCnpjs;
+
+    public async Task<DataTable> SiteCnpj(IPage page)
+    {
+        DataTable tabelaCnpjs = new();
+        tabelaCnpjs.Columns.Add("Cnpj", typeof(string));
+        tabelaCnpjs.Columns.Add("Status", typeof(string));
+
+        await CsvFile.WriteLineAsync("Cnpj;Status");
+
+        foreach (string cnpj in ListaCnpjs)
+        {
+            if (!Regex.IsMatch(cnpj, @"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$"))
+            {
+                tabelaCnpjs.Rows.Add(cnpj, "Cnpj não está no formato correto.");
+                continue;
+            }
+            string digitosCnpj = Regex.Replace(cnpj, @"\D", "");
+
+            await ConsultaCnpj(page, digitosCnpj);
+        }
+
+        return tabelaCnpjs;
+    }
+    private static async Task<bool> ConsultaCnpj(IPage page, string cnpj)
+    {
+        if (!await OpenCnpjSite(page))
+        {
+            throw new SiteNavigationException("Erro ao abrir o site da Fipe.");
+        }
+        return true;
+    }
+    private async Task<bool> OpenCnpjSite(IPage page)
+    {
+        int attempts = 0;
+        while (attempts < 5)
+        {
+            try
+            {
+                await page.GoToAsync(Url, 20000, WaitUntilNavigation.DOMContentLoaded);
+                if (await IsSiteLoaded(page))
+                    return true;
+            }
+            catch
+            {
+                attempts++;
+                await Task.Delay(5000);
+            }
+        }
+        return false;
+    }
+    private static async Task<bool> IsSiteLoaded(IPage page)
+    {
+        var elements = await page.XPathAsync("//*[@id=\"app\"]/div[1]/div/div/div/div/div");
+        return elements.Length > 0;
+    }
+}
---
+++ RPAs/ConsultaCnpj/ConsultaCnpj/Program.cs
@@ -0,0 +1,78 @@
+﻿using Application.Services;
+using PuppeteerExtraSharp;
+using PuppeteerSharp;
+using System.Data;
+
+string arquivoExcelCnpjs = string.Empty;
+string pastaTemp = string.Empty;
+
+#if RELEASE
+    arquivoExcelCnpjs = args[0];
+    pastaTemp = args[1];
+#else
+    arquivoExcelCnpjs = @"C:\temp\PlanilhaCnpjs.xlsx";
+    pastaTemp = $@"C:\temp\rpa\{Guid.NewGuid()}";
+#endif
+
+if (String.IsNullOrEmpty(arquivoExcelCnpjs))
+    throw new FileNotFoundException("Nenhum arquivo de CNPJs foi informado.");
+else if (!File.Exists(arquivoExcelCnpjs))
+    throw new FileNotFoundException("O arquivo de CNPJs informado não existe.");
+else if (String.IsNullOrEmpty(pastaTemp))
+    throw new FileNotFoundException("Nenhuma pasta temporária foi informada.");
+
+Directory.CreateDirectory(pastaTemp);
+if (!Directory.Exists(pastaTemp))
+    throw new FileNotFoundException("A pasta temporária informada não existe.");
+
+StreamWriter log = new(pastaTemp + @"\log.txt");
+StreamWriter csv = new(pastaTemp + @"\TabelaCnpj.csv");
+string url = "https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp";
+string csvPath = pastaTemp + @"\TabelaCnpj.csv";
+string msgExecucao = "INÍCIO";
+var puppeteer = new PuppeteerExtra();
+
+await new BrowserFetcher().DownloadAsync();
+
+var browser = await puppeteer.LaunchAsync(new LaunchOptions()
+{
+    DumpIO = true,
+    Headless = false,
+    Args = ["--start-maximized"]
+});
+
+var page = await browser.NewPageAsync();
+
+await page.SetViewportAsync(new ViewPortOptions()
+{
+    Width = 1024,
+    Height = 768,
+});
+
+try
+{
+    await log.WriteLineAsync(msgExecucao);
+    await log.WriteLineAsync("------------------------------------------------------------");
+
+    var listaCnpjs = new LerPlanilhaCnpjsService(arquivoExcelCnpjs).ObterListaDeCnpjs();
+    if (listaCnpjs.Count == 0)
+        throw new Exception("Não havia nenhum CNPJ para consulta no Excel informado.");
+
+    var tabelaCnpjs = await new ConsultaCnpjService(log, csv, url, listaCnpjs).SiteCnpj(page);
+
+    msgExecucao = "FIM";
+}
+catch (Exception ex)
+{
+    msgExecucao = $"Erro na execução: {ex.Message}";
+}
+finally
+{
+    await browser.CloseAsync();
+    csv.Close();
+    await csv.DisposeAsync();
+    //new ConverterCsvService(pastaTemp).ExportarParaExcel(csvPath);
+    await log.WriteLineAsync("------------------------------------------------------------");
+    await log.WriteLineAsync(msgExecucao);
+    await log.DisposeAsync();
+}
